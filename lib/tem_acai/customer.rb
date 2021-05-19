52e55f08bcc4d296b1589ce776953086d8bf2c1b
William Tomé <william.tomenh@gmail.com>
2021-05-19 18:26:35 -0300

    feat: return error message when phone is nil.

--- lib/tem_acai/customer.rb
+++ lib/tem_acai/customer.rb
@@ -4,6 +4,7 @@
 class Customer
   def self.create(name, phone)
     return "Por favor, informe seu nome." if name.nil?
+    return "Por favor, informe seu telefone." if phone.nil?
 
     { id: 1, name: name, phone: phone }
   end
