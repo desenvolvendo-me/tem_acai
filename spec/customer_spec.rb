abcc8acbf341b5352adfb4f64e01b6991985e7dd
William Tomé <william.tomenh@gmail.com>
2021-05-19 18:04:49 -0300

    docs: add comments.

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -1,10 +1,12 @@
-require 'tem_acai/customer'
+# frozen_string_literal: true
+
+require "tem_acai/customer"
 
 RSpec.describe "customer" do
   it "create" do
     name = "fulano"
     phone = "51992345856"
-    expect(Customer.create(name, phone)).to eq({id: 1, name: "fulano", phone: "51992345856"})
+    expect(Customer.create(name, phone)).to eq({ id: 1, name: "fulano", phone: "51992345856" })
   end
 
   it "throw error message if customer is no name" do
