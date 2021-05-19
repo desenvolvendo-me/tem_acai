3c560c511205775285185e9e62caff117944ba1a
Felipe Augusto Rosa <flp.far@gmail.com>
2021-05-18 22:23:32 -0300

    test: add spec for data persistance in company creation

--- spec/company_spec.rb
+++ spec/company_spec.rb
@@ -7,5 +7,22 @@ RSpec.describe Company do
       expect(company[:phone]).to eq("11-11111111")
       expect(company[:id]).to be_truthy
     end
+
+    it "without a phone" do
+      company = Company.create(name: "Casa do Açaí")
+
+      expect(company[:name]).to eq("Casa do Açaí")
+      expect(company[:phone]).to eq("")
+    end
+
+    it "persists the data" do
+      companies = Company.all
+      company = Company.create(name: "Casa do Açaí")
+      new_companies = Company.all
+
+      expect(companies).to be_empty
+      expect(new_companies.size).to eq 1
+      expect(new_companies.to_s).to include("Casa do Açaí")
+    end
   end
 end
