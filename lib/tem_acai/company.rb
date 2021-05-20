a3ccc6dbbdd4a980d7382e44ae3661938cd2b484
Felipe Augusto Rosa <flp.far@gmail.com>
2021-05-19 21:53:46 -0300

    feat: add company initializer

--- lib/tem_acai/company.rb
+++ lib/tem_acai/company.rb
@@ -4,20 +4,28 @@ require "csv"
 class Company
   DATA_PATH = "data/companies.csv"
 
+  attr_reader :id, :name, :phone
+
+  def initialize(id:, name:, phone: "")
+    @id = id
+    @name = name
+    @phone = phone
+  end
+
   def self.all
     companies = []
 
     CSV.read(DATA_PATH, headers: true).each do |row|
-      companies << { id: row["id"], name: row["name"], phone: row["phone"] }
+      companies << Company.new(id: row["id"], name: row["name"], phone: row["phone"])
     end
     companies
   end
 
   def self.create(name:, phone: "")
-    new_company = { id: 1, name: name, phone: phone }
+    new_company = Company.new(id: 1, name: name, phone: phone)
 
     CSV.open(DATA_PATH, "ab") do |csv|
-      csv << [new_company[:id], new_company[:name], new_company[:phone]]
+      csv << [new_company.id, new_company.name, new_company.phone]
     end
 
     new_company
--- spec/company_spec.rb
+++ spec/company_spec.rb
@@ -14,23 +14,25 @@ RSpec.describe Company do
     it "creates a company with id, name and phone" do
       company = Company.create(name: "Casa do Açaí", phone: "11-11111111")
 
-      expect(company[:name]).to eq("Casa do Açaí")
-      expect(company[:phone]).to eq("11-11111111")
-      expect(company[:id]).to be_truthy
+      expect(company.name).to eq("Casa do Açaí")
+      expect(company.phone).to eq("11-11111111")
+      expect(company.id).to be_truthy
     end
 
     it "without a phone" do
       company = Company.create(name: "Casa do Açaí")
 
-      expect(company[:name]).to eq("Casa do Açaí")
-      expect(company[:phone]).to eq("")
+      expect(company.name).to eq("Casa do Açaí")
+      expect(company.phone).to eq("")
     end
 
     it "persists the data" do
+      companies = Company.all
       Company.create(name: "Casa do Açaí")
       new_companies = Company.all
 
-      expect(new_companies.to_s).to include("Casa do Açaí")
+      expect(companies).to be_empty
+      expect(new_companies.first.name).to eq("Casa do Açaí")
     end
   end
 
