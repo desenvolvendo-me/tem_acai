83195bbcfb41c4e7880b73a85353d799073fbf06
Felipe Augusto Rosa <flp.far@gmail.com>
2021-05-21 06:58:15 -0300

    test: add spec for data persistance in rating

--- spec/rating_spec.rb
+++ spec/rating_spec.rb
@@ -20,6 +20,17 @@ RSpec.describe Rating do
       expect(rating.content).to eq("Muito bom!")
       expect(rating.id).to be_truthy
     end
+
+    it "persists the data" do
+      Rating.create(company_id: "123", customer_id: "21", rate: "8", content: "Muito bom!")
+      Rating.create(company_id: "123", customer_id: "26", rate: "1", content: "Péssimo")
+      Rating.create(company_id: "84", customer_id: "10", rate: "5", content: "Até que dá pro gasto")
+
+      ratings = Rating.all
+
+      expect(ratings.last.content).to eq("Até que dá pro gasto")
+      expect(ratings.last.rate).to eq("5")
+    end
   end
 
   def restart_csv(file_path)
