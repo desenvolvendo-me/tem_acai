88afc59dc8678fd0f6dd1c75894cba14cfef3b62
William Tomé <william.tomenh@gmail.com>
2021-05-20 21:54:52 -0300

    refactor: refactor code.

--- lib/tem_acai/customer.rb
+++ lib/tem_acai/customer.rb
@@ -4,16 +4,34 @@ require "csv"
 
 # Customer class
 class Customer
-  def self.create(name, phone)
-    return "Por favor, informe seu nome." if name.nil?
-    return "Por favor, informe seu telefone." if phone.nil?
+  ID_RANDOM_SET = 2000
+  DATA_PATH = "data/customers.csv"
 
-    { id: 1, name: name, phone: phone }
+  attr_reader :id, :name, :phone
+
+  def initialize(id:, name:, phone: "")
+    @id = id
+    @name = name
+    @phone = phone
+  end
+
+  def self.create(name, phone)
+    id = rand(2000)
+
+    customer = Customer.new(id: id, name: name, phone: phone)
+    return "O nome é obrigatório." if customer.name.nil? || customer.name == ""
+    return "O telefone é obrigatório." if customer.phone.nil? || customer.phone == ""
+
+    CSV.open(DATA_PATH, "ab") do |csv|
+      csv << [customer.id, customer.name, customer.phone]
+    end
+
+    customer
   end
 
   def self.all
     customers = []
-    CSV.read("data/customers.csv", headers: true).each do |row|
+    CSV.read(DATA_PATH, headers: true).each do |row|
       customers << { id: row["id"], name: row["name"], phone: row["phone"] }
     end
     customers
