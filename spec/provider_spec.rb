1c82c23ccd4b3aca35b8f7b72ba104d4bd0d3a1d
williamtome <9632783335245523>
2021-05-26 23:21:57 -0300

    test: create test provider phone as optional.

--- spec/provider_spec.rb
+++ spec/provider_spec.rb
@@ -17,10 +17,16 @@ RSpec.describe Provider do
     expect(provider.name).to eq("Fornecedor 1")
   end
 
+  it ".create phone" do
+    provider = Provider.create("Fornecedor 2")
+    expect(provider.phone).to be_nil
+  end
+
   it ".validate name is required" do
     expect(Provider.create(nil)).to eq("O nome é obrigatório.")
     expect(Provider.create("")).to eq("O nome é obrigatório.")
   end
+
 
   def restart_csv(file_path)
     CSV.open(file_path, "wb") do |csv|
