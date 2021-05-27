0e3afac4dcdcdb4268c4095f01361258c72371b3
William Tomé <william.tomenh@gmail.com>
2021-05-26 21:11:24 -0300

    test: create provider name test.

---
+++ spec/provider_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "tem_acai/provider"
+
+RSpec.describe Provider do
+  csv_path = "spec/support/providers-test.csv"
+
+  before do
+    stub_const("Provider::DATA_PATH", csv_path)
+    restart_csv(csv_path)
+  end
+
+  after(:all) { restart_csv(csv_path) }
+
+  it ".create name" do
+    provider = Provider.create( "Fornecedor 1")
+    expect(provider.name).to eq("Fornecedor 1")
+  end
+
+  def restart_csv(file_path)
+    CSV.open(file_path, "ab") do |csv|
+      csv << %w[id name]
+    end
+  end
+end
