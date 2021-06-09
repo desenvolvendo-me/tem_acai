fb47f7aaa47485567a8e3f607b24331c99a37b2e
paulo-felipe <alparakleto@gmail.com>
2021-06-09 07:20:54 -0300

    style(reservation_spec.rb): rubocop

--- spec/reservation_spec.rb
+++ spec/reservation_spec.rb
@@ -1,27 +1,29 @@
+# frozen_string_literal: true
+
 require "tem_acai/reservation"
 RSpec.describe Reservation do
-  context 'create' do
-    it 'be true' do
+  context "create" do
+    it "be true" do
       quantity = 2
       reservation = Reservation.new(quantity)
 
-      expect(reservation.quantity).to  eq(quantity)
+      expect(reservation.quantity).to eq(quantity)
     end
   end
 
-  context 'not create' do
-    it 'zero' do
+  context "not create" do
+    it "zero" do
       quantity = 0
       reservation = Reservation.new(quantity)
 
-      expect(reservation.quantity).to  be_nil
+      expect(reservation.quantity).to be_nil
     end
 
-    it 'negative' do
+    it "negative" do
       quantity = -1
       reservation = Reservation.new(quantity)
 
-      expect(reservation.quantity).to  be_nil
+      expect(reservation.quantity).to be_nil
     end
   end
 end
