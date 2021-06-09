97a6921bb427fcc0f513f7923c9e8834ca181ba7
paulo-felipe <alparakleto@gmail.com>
2021-06-09 07:21:19 -0300

    style(reservation.rb): rubocop

--- lib/tem_acai/reservation.rb
+++ lib/tem_acai/reservation.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 class Reservation
   attr_accessor :quantity
+
   def initialize(quantity)
     return nil if quantity.zero?
+    return nil if quantity.negative?
 
     @quantity = quantity
   end
