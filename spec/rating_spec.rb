857eea682125bd538de6c99936da3372d410c24c
paulo-felipe <alparakleto@gmail.com>
2021-06-10 05:55:44 -0300

    test(rating_spec.rb): add validation to rate attribute

--- spec/rating_spec.rb
+++ spec/rating_spec.rb
@@ -14,11 +14,31 @@ RSpec.describe Rating do
     it "creates a rating for the company" do
       rating = Rating.create(company_id: "123", customer_id: "21", rate: "8", content: "Muito bom!")
 
+      expect(rating.valid?).to eq(true)
       expect(rating.company_id).to eq("123")
       expect(rating.customer_id).to eq("21")
       expect(rating.rate).to eq("8")
       expect(rating.content).to eq("Muito bom!")
       expect(rating.id).to be_truthy
+    end
+
+    it "persists the data" do
+      Rating.create(company_id: "123", customer_id: "21", rate: "8", content: "Muito bom!")
+      Rating.create(company_id: "123", customer_id: "26", rate: "1", content: "Péssimo")
+      Rating.create(company_id: "84", customer_id: "10", rate: "5", content: "Até que dá pro gasto")
+
+      ratings = Rating.all
+
+      expect(ratings.last.content).to eq("Até que dá pro gasto")
+      expect(ratings.last.rate).to eq("5")
+    end
+  end
+
+  context "not create" do
+    it "without rate" do
+      rating = Rating.create(company_id: "123", customer_id: "21", rate: nil, content: "Muito bom!")
+
+      expect(rating.valid?).to eq(false)
     end
 
     it "persists the data" do
