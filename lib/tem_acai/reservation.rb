97687cdf2b14de3210588ea299d90cd8389aaa35
Diego Araujo <diego_shakan@hotmail.com>
2021-06-10 20:14:54 -0300

    feat(company): save data in csv

--- lib/tem_acai/reservation.rb
+++ lib/tem_acai/reservation.rb
@@ -1,10 +1,19 @@
 # frozen_string_literal: true
 
 class Reservation
+  DATA_PATH = "data/reservations.csv"
   attr_accessor :quantity, :customer_id, :company_id, :time_to_take
 
-  def initialize(quantity)
+  def initialize(quantity, id: rand(200))
+    @id = id
     @quantity = quantity
+  end
+
+  def create
+    CSV.open(DATA_PATH, "ab") do |csv|
+      csv << [id, customer_id, company_id, quantity, time_to_take]
+    end
+    self
   end
 
   def valid?
