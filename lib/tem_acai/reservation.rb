2840d9b74e3fcc5bd206f4f63cb1a2ebb7075d82
Diego Araujo <diego_shakan@hotmail.com>
2021-06-10 20:05:19 -0300

    feat(company): add new fields

--- lib/tem_acai/reservation.rb
+++ lib/tem_acai/reservation.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Reservation
-  attr_accessor :quantity
+  attr_accessor :quantity, :customer_id, :company_id, :time_to_take
 
   def initialize(quantity)
     @quantity = quantity
