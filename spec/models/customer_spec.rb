a489372f7fafec302127e4ec8713f230a6527bcd
Felipe Augusto Rosa <flp.far@gmail.com>
2021-06-17 06:20:21 -0300

    test: customer presence validations

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -3,5 +3,8 @@
 require "rails_helper"
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:phone) }
+  end
 end
