fded9cd748b17102842ddc48b99012db37b51777
Felipe Augusto Rosa <flp.far@gmail.com>
2021-06-18 06:46:53 -0300

    feat: rating validations

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,4 +3,7 @@
 class Rating < ApplicationRecord
   belongs_to :customer
   belongs_to :company
+
+  validates :rate, presence: true,
+                   numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end
