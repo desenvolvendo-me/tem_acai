6b24ad38ff97f9aa21ee8499c9acb80c167bc693
Felipe Augusto Rosa <flp.far@gmail.com>
2021-06-18 06:56:43 -0300

    test: rating validations with shoudamatchers

--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -5,37 +5,10 @@ require "rails_helper"
 RSpec.describe Rating, type: :model do
   describe "validations" do
     context "rate" do
-      it "is valid" do
-        rating = build(:rating, rate: 4, content: "Very good!")
-
-        expect(rating.valid?).to eq(true)
-      end
-
       it { should validate_presence_of(:rate) }
-
-      it "is expected to be an integer" do
-        rating = build(:rating, rate: 1.25)
-
-        rating.valid?
-
-        expect(rating.errors[:rate]).to include("must be an integer")
-      end
-
-      it "is expected to be less or equal to 5" do
-        rating = build(:rating, rate: 6)
-
-        rating.valid?
-
-        expect(rating.errors[:rate]).to include("must be less than or equal to 5")
-      end
-
-      it "is expected to be greater or equal to 1" do
-        rating = build(:rating, rate: 0)
-
-        rating.valid?
-
-        expect(rating.errors[:rate]).to include("must be greater than or equal to 1")
-      end
+      it { should allow_values(1, 2, 3, 4, 5).for(:rate) }
+      it { should_not allow_value(2.5).for(:rate) }
+      it { should_not allow_value(-1, 0, 6).for(:rate) }
     end
   end
 
