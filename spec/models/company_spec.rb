f1a7dff3802f9cbcaabedcd6140770aab99d2f28
Felipe Augusto Rosa <flp.far@gmail.com>
2021-06-17 23:02:32 -0300

    test: company validations and default specs

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -3,5 +3,31 @@
 require "rails_helper"
 
 RSpec.describe Company, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+
+    it "phone is required if delivery is true" do
+      company = Company.new(name: "New Company", delivery: true)
+
+      company.valid?
+
+      expect(company.errors["phone"]).to eq(["can't be blank"])
+    end
+  end
+
+  describe "default values" do
+    let(:company) { Company.create(name: "New Company") }
+
+    it "is_open is expected to be false by default" do
+      expect(company.is_open).to eq(false)
+    end
+
+    it "reservation is expected to be false by default" do
+      expect(company.reservation).to eq(false)
+    end
+
+    it "deliver is expected to be false by default" do
+      expect(company.delivery).to eq(false)
+    end
+  end
 end
