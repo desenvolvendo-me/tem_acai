d1142a0aa772a0481fb80376d31c1a57f4c746b4
Felipe Augusto Rosa <flp.far@gmail.com>
2021-06-24 07:51:28 -0300

    feat: company inform_open api

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :companies, only: %i[index create]
+      resources :companies, only: %i[index create] do
+        patch "inform_open"
+      end
     end
   end
 end
